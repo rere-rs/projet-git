97cfbc211a1c7b469e3d26f36da44dcc72d846cb
rere-rs <75220548+rere-rs@users.noreply.github.com>
2023-11-27 22:13:07 +0100

    feat: ajouter une fonction pour dessiner une droite (#12)
    Cette fonction permet de dessiner une droite
    
    #7

--- paint.c
+++ paint.c
@@ -8,8 +8,13 @@ void effacer_dessin() {
   printf("Dessin effacé");
 }
 
+void dessiner_droite(int k) {
+  int k = 3;
+}
+
 int main(void) {
   dessiner_carre(i)
+  dessiner_droite(k);
   effacer_dessin();
   return 0;
 }
