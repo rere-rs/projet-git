6a237eff2df19630faaea7380154e205178913dd
s3d1p0x <quentin.auspitz@gmail.com>
2023-11-28 16:52:32 +0100

    feat: ajouter une fonction pour dessiner une droite (#14) (#15)

--- paint.c
+++ paint.c
@@ -8,6 +8,10 @@ void effacer_dessin() {
   printf("Dessin effacé");
 }
 
+void dessiner_rectangle(int j) {
+  int j = 3;
+}
+
 void dessiner_droite(int k) {
   int k = 3;
 }
@@ -15,6 +19,7 @@ void dessiner_droite(int k) {
 int main(void) {
   dessiner_carre(i)
   dessiner_droite(k);
+  dessiner_rectangle(j);
   effacer_dessin();
   return 0;
 }
