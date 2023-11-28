c855bdfcd0b5bc51d9d15107d811e1501ccf8d08
s3d1p0x <quentin.auspitz@gmail.com>
2023-11-28 17:00:36 +0100

    Ajout de la fonction drawCircle (#16)
    Cette fonction permet de dessiner un cercle

--- paint.c
+++ paint.c
@@ -16,10 +16,17 @@ void dessiner_droite(int k) {
   int k = 3;
 }
 
+void drawCircle(x, y, radius){
+  int x = 2;
+  int y = 4;
+  int radius = 5;
+}
+
 int main(void) {
   dessiner_carre(i)
   dessiner_droite(k);
   dessiner_rectangle(j);
+  drawCircle(x, y radius);
   effacer_dessin();
   return 0;
 }
