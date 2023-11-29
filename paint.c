6810b7d92f1e8f564db979284677dd7eee1f9a57
rere-rs <regissem@outlook.com>
2023-11-29 12:28:30 +0100

    merge: Préparation de la fusion de la branche "develop" vers "master" pour la production (#22)
    * Ajout de l'exécutable et de la fonction main (#3)
    
    * Ajout du fichier principal de Paint (#1)
    
    Le fichier paint.c permet d'ajouter toute les fonctions pour faire fonctionner Paint
    
    * Ajout de l'executable de paint.c (#2)
    
    L'executable paint.exe permet de lancher l'application paint.
    
    ---------
    
    Co-authored-by: rere-rs <75220548+rere-rs@users.noreply.github.com>
    
    * Préparation de la fusion de la branche "feature" vers la branche "develop" (#21)
    
    * Ajout du fichier principal de Paint (#1)
    
    Le fichier paint.c permet d'ajouter toute les fonctions pour faire fonctionner Paint
    
    * Ajout de l'executable de paint.c (#2)
    
    L'executable paint.exe permet de lancher l'application paint.
    
    * feat: ajout de la fonction "effacer" (#10)
    
    Cette fonctionnalité permet d'effacer les dessins.
    
    #4
    
    * feat: ajouter une fonction pour dessiner une droite (#12)
    
    Cette fonction permet de dessiner une droite
    
    #7
    
    * feat: ajouter une fonction pour dessiner une droite (#14) (#15)
    
    * Ajout de la fonction drawCircle (#16)
    
    Cette fonction permet de dessiner un cercle
    
    * feat: ajout d'une palette de couleur (#18)
    
    Cette fonction permet de choisir une couleur via une palette intéractive.
    
    close #9
    
    * feat: ajout d'une fonction pour dessiner triangles (#19) (#20)
    
    Cette fonction ajoute la possibilité de dessiner des triangles
    
    ---------
    
    Co-authored-by: rere-rs <75220548+rere-rs@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: s3d1p0x <quentin.auspitz@gmail.com>

--- paint.c
+++ paint.c
@@ -4,6 +4,43 @@ void dessiner_carre(int i) {
   int i = 1;
 }
 
+void effacer_dessin() {
+  printf("Dessin effacé");
+}
+
+void dessiner_rectangle(int j) {
+  int j = 3;
+}
+
+void dessiner_droite(int k) {
+  int k = 3;
+}
+
+void drawCircle(x, y, radius){
+  int x = 2;
+  int y = 4;
+  int radius = 5;
+}
+
+void dessiner_triangle(a, b, c){
+  int a = 2;
+  int b = 2;
+  int c = 5;
+}
+
+void palette_couleur(couleur){
+  couleur
+}
+
+int main(void) {
+  dessiner_carre(i)
+  dessiner_droite(k);
+  dessiner_rectangle(j);
+  drawCircle(x, y radius);
+  dessiner_triangle(a, b, c)
+  palette_couleur(couleur);
+  effacer_dessin();
+  
 int main(void) {
   dessiner_carre(i)
   return 0;
